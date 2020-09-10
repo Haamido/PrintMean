d47bc7acbdddce7425e22630208824bc0dc419f3
hamid <hamid.nazarifar@student.kyh.se>
2020-09-10 11:46:11 +0200

    inital commt

---
+++ PrintMean.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PrintMean {
+
+    public static void main(String[] args) {
+        System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(a: 1, b: 2, c:3, d:4));
+        System.out.println("Mean of 1, 1, 3, 3: " +  meanCalc(a: 1, b: 2, c:3, d:4));
+        System.out.println("Mean of 1, 1, 3, 3: " +  meanCalc(a: 1, b: -2, c: 3, d:-4));
+        System.out.println("Mean of -1, -11, 24, 2: " +  meanCalc(a: -1, b: -11, c: 24, d:2));
+    }
+
+    public static String meanCalc(int a, int b, int c, int d) {
+        int mean = (a + b + c + d) / 4;
+        String meanString = "";
+
+        return meanString;
+
+    }
+}
