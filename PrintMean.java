e1bd667590f7e9bbc9e9c0424437eb4eeed541e7
hamid <hamid.nazarifar@student.kyh.se>
2020-09-10 13:40:31 +0200

    commit

--- PrintMean.java
+++ PrintMean.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PrintMean {
 
     public static void main(String[] args) {
-        // testar detta bara. 
+        // testar detta bara.
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(a: 1, b: 2, c:3, d:4));
         System.out.println("Mean of 1, 1, 3, 3: " +  meanCalc(a: 1, b: 2, c:3, d:4));
         System.out.println("Mean of 1, 1, 3, 3: " +  meanCalc(a: 1, b: -2, c: 3, d:-4));
